e61eb01e780262872a38dfc379ef042b7ba4a6e3
Moisas <mopri08@gmail.com>
2021-11-12 18:39:07 -0300

    terminado a logica do react-countries

--- 03-react-countries/react-countries/src/components/Countries.jsx
+++ 03-react-countries/react-countries/src/components/Countries.jsx
@@ -1,13 +1,5 @@
-import Country from "./Country"
-export default function Countries({children: countries = []}) {
-    let qtdePais = countries.length
+export default function Countries({children}) {
     return (
-        <div className='border p-2'>
-            <h2 className='text-center'>{qtdePais > 1 ? qtdePais +' países' : qtdePais +' país'}</h2>
-
-            {countries.map(country => {
-               return <Country key={country.id}>{country}</Country>
-            })}
-        </div>
+        <div className='border p-2'>{children}</div>
     )
 }
--- 03-react-countries/react-countries/src/pages/ReactCountriesPage.jsx
+++ 03-react-countries/react-countries/src/pages/ReactCountriesPage.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react";
 import Countries from "../components/Countries";
+import Country from "../components/Country";
 import Header from "../components/Header";
 import Main from '../components/Main'
 import TextInput from '../components/TextInput'
 import { allCountries } from '../data/countries'
 
 export default function ReactCountriesPages() {
-  const [countryFilter, setCountryFilter] = useState('Brazil')
+  const [countryFilter, setCountryFilter] = useState('')
+  const [visitedCountries, setVisitedCountries] = useState([])
 
   function handleCountryFilterChange(newCountryFilter) {
     setCountryFilter(newCountryFilter);
+  }
+
+  function toggleVisitedCountry(countryId){
+    let newVisistedCountries = [...visitedCountries]
+
+    const isCountryVisited = newVisistedCountries.indexOf(countryId) !== -1
+
+    if(isCountryVisited){
+      newVisistedCountries = newVisistedCountries.filter(visitedCountryId => visitedCountryId !== countryId)
+    }else{
+      newVisistedCountries.push(countryId)
+    }
+    setVisitedCountries(newVisistedCountries)
   }
 
   const countryFilterLowercase = countryFilter.trim().toLocaleLowerCase()
 
   const filtredCountries = countryFilterLowercase.length >= 3 ?
    allCountries.filter(({nameLowerCase}) => nameLowerCase.includes(countryFilterLowercase)) : allCountries
-
+   let qtdePais = filtredCountries.length
+   let qtdeVisitados = visitedCountries.length
   return (
     <div>
       <Header>React Countries</Header>
@@ -29,10 +45,18 @@ export default function ReactCountriesPages() {
           onInputChange={handleCountryFilterChange}
           autoFocus
           />
-        <Countries>{filtredCountries}</Countries>
+        {/* <Countries visitedCountries={visitedCountries} onCountryClick={toggleVisitedCountry}>{filtredCountries}</Countries> */}
+        
+        <Countries>
+        
+        <h2 className='text-center font-semibold'>{qtdePais > 1 ? qtdePais +' países' : qtdePais +' país'}</h2>
+        <h3 className='text-center font-semibold text-sm'>{qtdeVisitados > 1 ? qtdeVisitados +' países vistados' : qtdeVisitados + ' país visitado'}</h3>
+        {filtredCountries.map(country => {
+                const isVisited = visitedCountries.indexOf(country.id) !== -1
+               return <Country isVisited ={isVisited} onCountryClick={toggleVisitedCountry} key={country.id}>{country}</Country>
+            })}
+        </Countries>
       </Main>
-
-      
     </div>
   )
 }
